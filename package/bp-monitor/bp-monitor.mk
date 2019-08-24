760c5ddea517dc2056a2fa97e34b0d3b5eeb198f
Wariston Pereira <waristonfernando@gmail.com>
2019-08-24 15:14:01 -0300

    Add BUILDROOT package.

---
+++ package/bp-monitor/bp-monitor.mk
@@ -0,0 +1,18 @@
+#
+# Build and install Blood Pressure Libs
+# @autor Wariston Pereira <waristonfernando@gmail.com>
+#
+BP_MONITOR_VERSION = 0.1
+BP_MONITOR_SITE = .
+BP_MONITOR_METHOD = local
+BP_MONITOR_OVERRIDE_SRCDIR = ../src/
+
+define BP_MONITOR_BUILD_CMDS
+    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
+endef
+
+define BP_MONITOR_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/libbpmonitor.so $(TARGET_DIR)/lib
+endef
+
+$(eval $(generic-package))
